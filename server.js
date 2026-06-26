2fa56b32ac90d5cca74169192bb92aea90510c30
ljp-akp <811322517@qq.com>
2026-06-27 03:43:27 +0800

    更新 server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.post("/v1/chat/completions", async (req, res) => {
 const response = await fetch("https://aix.6os.net/v1/chat/completions", {
   method: "POST",
   headers: {
-    "Authorization": "Bearer sk-rTQKF5k81tKU9YMNllA3H0DDxuyojLhgo88sbXWDTxdPQnMH",
+"Authorization": `Bearer ${process.env.API_KEY}`,
     "Content-Type": "application/json"
   },
   body: JSON.stringify({
